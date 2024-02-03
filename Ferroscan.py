049159135ffa341c9a521ccb5d51d2833a74e06e
Fredy Piraquive <40009527+Judiu@users.noreply.github.com>
2024-02-02 22:17:20 -0500

    Update Ferroscan.py

--- Ferroscan.py
+++ Ferroscan.py
@@ -1,11 +1,11 @@
 # Extraer informacion del PDF de Ferroscan enviados por PROFIS Detection
 
-from  PyPDF2 import PdfFileReader
+from  PyPDF2 import PdfReader
 
 def extraer_informacion (pdf_path):
     with open(pdf_path,  'rb') as f:
-        pdf = PdfFileReader(f)
-        num_pages = pdf.numPages
+        pdf = PdfReader(f)
+        num_pages = len(PdfReader.pages)
         information = pdf.getDocumentInfo()
 
     txt = f"""
@@ -16,12 +16,12 @@ def extraer_informacion (pdf_path):
     Producer: {information.producer}
     Subject: {information.subject}
     Title: {information.title}
-    Number of pages: {number_of_pages}
+    Number of pages: {num_pages}
     """
 
     print(txt)
     return information
 
 if __name__ == '__main__':
-    path = 'reportlab-sample.pdf'
+    path = 'E:\LPS Ingenieria Estructural\LPS Ingenieria Estructural - Ferroscan\Informes Entrega Final\CANAPRO PH\DatosCompleto.pdf'
     extraer_informacion(path)
